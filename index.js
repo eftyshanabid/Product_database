922a45518dc2e5ebda2c203c10a4dfeb05b27272
efty <eftyshanabid20@gmail.com>
2026-02-22 13:48:52 +0600

    further changes saved

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const env = require('dotenv');
+const ProductRoute = require('./routes/ProductRoute')
 env.config({path: './config/.env'});
 
 const app = express();
-const _port = process.env.PORT
+app.use(express.json());
+const _port = process.env.PORT;
+
+app.use('/product',ProductRoute)
 
 app.listen(_port,()=>{
     console.log(`app is running on ${_port}`)
