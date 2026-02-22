3e2c516b1c0db15c4627307070e3e2087e66e6b4
efty <eftyshanabid20@gmail.com>
2026-02-22 23:10:30 +0600

    deleteCustomer and customerById func added

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -33,4 +33,50 @@ async function getAllCustomer(req,res) {
     }
 }
 
-module.exports = {createCustomer,getAllCustomer}
+async function deleteCustomer(req,res) {
+    try{
+        const {id} = req.params;
+        const customer = await Customer.findByPk(id);
+        if(!customer)
+        {
+            res.status(404).json({
+                msg: `customer ${id} did not found`
+            })
+        }
+        await customer.destroy();
+        res.status(200).json({
+            msg: `customer with ${id} deleted`
+        })
+    }
+    catch(err){
+        console.log("Can not delete customer",err);
+        res.status(500).json({
+            msg: "Internal server error"
+        })
+    }
+}
+
+async function customerById(req,res) {
+    try{
+        const {id} = req.params;
+        const customer = await Customer.findByPk(id);
+        if(!customer)
+        {
+            res.status(404).json({
+                msg: `customer with ${id} did not found`
+            })
+        }
+        res.status(200).json({
+            customer: customer
+        })
+    }
+    catch(err)
+    {
+        console.log("error can not find customer",err);
+        res.status(500).json({
+            msg: `Internal server error`
+        })
+    }
+}
+
+module.exports = {createCustomer,getAllCustomer,deleteCustomer,customerById};
--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createCustomer, getAllCustomer } = require('../controller/CustomerController');
+const { createCustomer, getAllCustomer, deleteCustomer, customerById } = require('../controller/CustomerController');
 const router = express.Router();
 
 router.post('/create',createCustomer);
 router.get('/getAll',getAllCustomer);
+router.delete('/delete/:id',deleteCustomer);
+router.get('/customer/:id',customerById)
 
 module.exports = router;
