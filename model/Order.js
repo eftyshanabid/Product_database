9bde52e8531f046e25765cf8e3124be029eb2711
efty <eftyshanabid20@gmail.com>
2026-02-22 14:25:48 +0600

    order model added

---
+++ model/Order.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const DB = require('../database/DB');
+
+const Order = DB.define('Order',{
+    order_date:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    customer_id:{
+        type: DataTypes.INTEGER,
+        references:{
+            model: "Customers",
+            key: "id"
+        }
+    },
+    delivery_address:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    city:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+Order.sync()
+.then(()=>{
+    console.log("Order table created successfully")
+})
+.catch((err)=>{
+    console.log('Error creating Order table',err)
+})
